bb3a1b5ed8c46aa14af1f2adcb9c95aa1b0efc6b
Carlos Boyzo <carlosboyzo@iMac-de-Carlos.local>
2021-07-28 15:24:24 -0500

    hero

--- src/views/Home.js
+++ src/views/Home.js
@@ -1,16 +1,17 @@
 import React from "react";
-import Logo from "../assets/GoPharmaWhite.svg"
+import Logo from "../assets/GoPharmaLogo.png"
 function Home() {
   return (
     <React.Fragment>
-      <section>
+      <section className="hero">
         <div className="contenedor">
          
         <img
+        className="logo-hero"
             src={Logo}
             alt="Somos una agencia de CONSULTORÍA, MKT- MEDIA especializada en el mercado PHARMA y el sector SALUD."
           />
-            <h1>GO-PHARMA</h1>
+            
         </div>
       </section>
     </React.Fragment>
